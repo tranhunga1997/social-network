09f7b274a0dcd3bdfd087038316929a41ab12726
thuong <thuong@THUONG_01>
2022-03-23 23:52:02 +0900

    2022/03/23 23:51 thuong

--- src/main/java/com/socialnetwork/common/entities/BaseEntity.java
+++ src/main/java/com/socialnetwork/common/entities/BaseEntity.java
@@ -3,6 +3,7 @@ package com.socialnetwork.common.entities;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 import lombok.Data;
@@ -14,7 +15,10 @@ public class BaseEntity implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -1876453224787306485L;
+	@Column(name="create_at")
 	private LocalDateTime createDatetime;
+	@Column(name="update_at")
 	private LocalDateTime updateDatetime;
+	@Column(name="del_at")
 	private LocalDateTime deleteDatetime;
 }
