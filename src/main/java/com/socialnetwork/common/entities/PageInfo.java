5344a251aff26e284bf5aa5114116e3028dce3b6
thuong <thuong@THUONG_01>
2022-03-25 21:02:18 +0900

    2022/03/25 21:02 thuong

--- src/main/java/com/socialnetwork/common/entities/PageInfo.java
+++ src/main/java/com/socialnetwork/common/entities/PageInfo.java
@@ -51,7 +51,7 @@ public class PageInfo {
 	@Convert(converter = BooleanConvert.class)
 	private boolean delFlag;
 	//del_at
-	@Column(name="update_at")
+	@Column(name="del_at")
 	private LocalDateTime delAt;
 	//block_flag
 	@Column(name="block_flag", nullable = false)
--- src/main/java/com/socialnetwork/page/service/PageService.java
+++ src/main/java/com/socialnetwork/page/service/PageService.java
@@ -4,6 +4,9 @@ package com.socialnetwork.page.service;
  * @author thuong
  *
  */
+
+import com.socialnetwork.page.dto.PageDto;
+
 public interface PageService {
-	
+	PageDto findPageById(Long pageId);
 }
