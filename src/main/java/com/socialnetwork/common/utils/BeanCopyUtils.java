681a4f239da446d3492b4fe61a5f56454ba32dd0
thuong <thuong@THUONG_01>
2022-04-02 00:00:16 +0900

    update BeanCopyUtils (thuong)

--- src/main/java/com/socialnetwork/common/utils/BeanCopyUtils.java
+++ src/main/java/com/socialnetwork/common/utils/BeanCopyUtils.java
@@ -1,10 +1,7 @@
 package com.socialnetwork.common.utils;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 /**
  * sao chép thuộc tính
@@ -13,74 +10,58 @@ import java.util.Map;
  */
 public class BeanCopyUtils {
 	/**
-	 * Sao chép thuộc tính từ source sang target
+	 * Sao chép thuộc tính từ source sang target, loại trừ các giá trị null trong source
 	 * @param source
 	 * @param target
 	 */
 	public static void copyProperties(Object source, Object target) {
-		for(Field sourceFiled : source.getClass().getDeclaredFields()) {
-			try {
-				//Lấy thuộc tính cùng tên
-				Field targetField = target.getClass().getDeclaredField(sourceFiled.getName());
-				//Cho phép truy cập
-				sourceFiled.setAccessible(true);
-				targetField.setAccessible(true);
-				// copy
-				targetField.set(target, sourceFiled.get(source));
-			}catch (Exception e) {
-				//nothing
-			}
-		}
+		copyProperties(source, target, false);
 	}
 	public static void copyProperties(Object source, Object target, boolean copyNull) {
-		for(Field sourceFiled : source.getClass().getDeclaredFields()) {
-			try {
-				//Lấy thuộc tính cùng tên
-				Field targetField = target.getClass().getDeclaredField(sourceFiled.getName());
-				//Cho phép truy cập
-				sourceFiled.setAccessible(true);
-				targetField.setAccessible(true);
-				// copy
-				Object value = sourceFiled.get(source);
-				if(!copyNull && value == null)
+		// Lấy fields của source
+		Map<String, Field> sourceFields = new HashMap<String, Field>();
+		Class<?> tempClass = source.getClass();
+		while(tempClass!=null && !tempClass.equals(Object.class)) {
+			for(Field f: tempClass.getDeclaredFields()) {
+				// kiểm tra đã tồn tại field hay chưa (xảy ra khi trùng tên field của super)
+				if(sourceFields.containsKey(f.getName()))
 					continue;
-				targetField.set(target, value);
-			}catch (Exception e) {
-				//nothing
+				// loại bỏ các modifier final, static...
+				if(f.getModifiers()>=4)
+					continue;
+				sourceFields.put(f.getName(), f);
 			}
 		}
-	}
-	/**
-	 * chuyển dữ liệu sang dạng map
-	 * @param source
-	 * @return
-	 */
-	public static Map<String, Object> getMapFromProperties(Object source, String...propertyNames){
-		Map<String, Object> map = new HashMap<String, Object>();
-		List<Field> fields; 
-		if(propertyNames.length ==0)
-			fields = Arrays.asList(source.getClass().getDeclaredFields());
-		else {
-			fields = new ArrayList<Field>();
-			for(String fieldName : propertyNames) {
-				try {
-					fields.add(source.getClass().getField(fieldName));
-				}catch (Exception e) {
-					//không tồn tại thuộc tính
-					//throw new IllegalArgumentException("property not found");
-				}
+		// Lấy fields của target
+		Map<String, Field> targetFields = new HashMap<String, Field>();
+		tempClass = target.getClass();
+		while(tempClass!=null && !tempClass.equals(Object.class)) {
+			for(Field f: tempClass.getDeclaredFields()) {
+				// kiểm tra đã tồn tại field hay chưa (xảy ra khi trùng tên field của super)
+				if(targetFields.containsKey(f.getName()))
+					continue;
+				// loại bỏ các modifier final, static...
+				if(f.getModifiers()>=4)
+					continue;
+				targetFields.put(f.getName(), f);
 			}
 		}
-		
-		for(Field field: fields) {
+		// copy thuộc tính cùng tên
+		for(String name : sourceFields.keySet()) {
+			Field targetField = targetFields.get(name);
+			Field sourceField = sourceFields.get(name);
+			if(targetField== null)
+				continue;
 			try {
-				field.setAccessible(true);
-				map.put(field.getName(), field.get(source));
-			}catch(Exception e) {
-				// Không lấy được dữ liệu
-				//throw new IllegalAccessError("Can't access to property");
+				targetField.setAccessible(true);
+				sourceField.setAccessible(true);
+				Object value = sourceField.get(source);
+				if(value != null || copyNull)
+					targetField.set(target, value);
+			}catch (Exception e) {
+				// Nothing
 			}
+			
 		}
-		return map;
 	}
 }
